37ab69ac0e9995fc0fa81f3f430850ae5d8cf7db
wions37 <gpdnjs0307@gmail.com>
2024-05-30 13:55:52 +0900

    init

--- src/Test530.java
+++ src/Test530.java
@@ -1,5 +1,27 @@
 public class Test530 {
-    public static void main(String[] args) {
-        System.out.println("helloworld");
-    }
+  public static void main(String[] args) {
+//    int a = 20;
+//    int b = 30;
+//    int c = 50;
+//    System.out.println(a + b + c);
+
+    char c1 = 'A';
+    char c2 = 65;
+    char c3 = '\u0041';
+
+    char c4 = '가';
+    char c5 = 44032;
+    char c6 = '\uac00';
+
+    int uniCode = c1;
+
+    System.out.println(c1);
+    System.out.println(c2);
+    System.out.println(c3);
+    System.out.println(c4);
+    System.out.println(c5);
+    System.out.println(c6);
+    System.out.println(uniCode);
+
+  }
 }
