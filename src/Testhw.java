ecfa3df229a4b79a4f5d33d5fb6f19175529abc6
wions37 <gpdnjs0307@gmail.com>
2024-05-30 10:28:39 +0900

    init

---
+++ src/Testhw.java
@@ -0,0 +1,20 @@
+public class Testhw {
+    /*    public static void main(String[] args) {
+            System.out.println("hello world1"); //출력하는라인
+        }*/
+    /**
+    * @since 8240603 이날짜부터 사용가능
+    * @author hyewon 만든사람
+    * @apiNote 무슨 기능을 하는지
+    *
+    *
+    * wntjr
+    * wntjr
+    * 주석
+    *
+    * */
+    public static void main(String[] args) {
+        System.out.println(1+2);
+        System.out.println(3+4);
+    }
+}
