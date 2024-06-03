b94cdd618117ceb16cc1fede60c4bf060f497ee2
wions37 <gpdnjs0307@gmail.com>
2024-06-03 17:35:10 +0900

    init

---
+++ src/test603.java
@@ -0,0 +1,122 @@
+import java.sql.SQLOutput;
+
+public class test603 {
+  public static void main(String[] args) {
+    //        x > 5 && x < 13
+    //        i % 2 == 0 || i % 3 ==0;
+    //        i % 2 == 0 && i % 3 ==0;
+    //        ( x % 30 ==0 && x % 2==0) || x % 5 == 0;
+    //        System.out.println();
+    //        int score = 95;
+    //        char grade = (score > 90) ? 'A' : 'B';
+    //    int score = 95;
+    //    String grade = (score > 95) && (score % 5 == 0) ? "Good" : "Bad";
+    //    System.out.println(grade);
+
+    //    if (score > 90) {
+    //      System.out.println("good");
+    //    } else {
+    //      System.out.println("bad");
+    //    }
+//    int a = 10;
+//    int b = 5;
+//    if (a > b) {
+////      System.out.println("a is greater than b");
+//    }
+//    else if (a == b) {
+//      System.out.println("a and b are equal");
+//    }
+//    else {
+//      System.out.println("a is less than b");
+//    }
+
+//    System.out.println((a>b)?"a is greater than b":(a == b)?"a and b are equal":"a is less than b");
+//
+//    int a = 80;
+//    if (a > 90) {
+//      System.out.println("A");
+//    }
+//    else if (a > 80) {
+//      System.out.println("B");
+//    }
+//    else if (a > 70) {
+//      System.out.println("C");
+//    }
+////    else if (a > 60) {
+//      System.out.println("D");
+//    }
+//    else {
+//      System.out.println("F");
+////    }
+//////-----/
+//
+////    1번
+//
+//    int x = 10;
+//
+//    System.out.println(x > 0 ? "양수" : x < 0 ? "양수" : 0);
+//
+//    //    2번
+//    int score = 75;
+//
+//    if (score >= 90) {
+//      System.out.println("A");
+//    } else if (score >= 80) {
+//      System.out.println("B");
+//    } else if (score >= 70) {
+//      System.out.println("C");
+//    } else if (score >= 60) {
+//      System.out.println("D");
+//    } else {
+//      System.out.println("F");
+//    }
+////    3번
+//    int year = 2023;
+//    System.out.println(year % 4 == 0 && (year % 100 != 0 || year % 400 == 0) ? "윤년" : "평년");
+////    4번
+//    int a = 10;
+//    int b = 20;
+//    int c = 30;
+//    System.out.println(a > b ? a : b > c ? b : c);
+////    5번
+//    int passScore = 60;
+//    int myScore = 75;
+//    System.out.println(passScore > myScore ? "불합격" : "합격");
+
+//      int a = 10;
+//      int b = 5;
+//
+//      boolean result1 = (a<5) && (b<10);
+//      boolean result2 = (a<5) || (b<10);
+//      boolean result3 = !(a<5);
+//
+//    System.out.println(result1);
+//    System.out.println(result2);
+//    System.out.println(result3);
+
+//    { String day = "목요일";
+//    switch (day){
+//      case "월요일":
+//        System.out.println("월요일 입니다.");
+//        break;
+//      case "화요일":
+//        System.out.println("화요일 입니다.");
+//        break;
+//      case "수요일":
+//        System.out.println("수요일 입니다.");
+//        break;
+//      case "목요일":
+//        System.out.println("목요일 입니다.");
+//        break;
+//      case "금요일":
+//        System.out.println("금요일 입니다.");
+//        break;
+//      case "토요일":
+//        System.out.println("토요일 입니다.");
+//        break;
+//      case "일요일":
+//        System.out.println("일요일 입니다.");
+//        break;
+  }
+}
+}
